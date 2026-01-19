aa66a251ff6b885d1f0811b57267be2f55caf660
Dilagante <dinethdds@gmail.com>
2026-01-19 11:57:55 +0530

    feat(router): restructure App component to implement routing for editor and playout pages

--- graphyne-client/src/App.tsx
+++ graphyne-client/src/App.tsx
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { TestArtboard } from './tests/TestArtboard';
-import './App.css';
+import { TestArtboard } from './tests/TestArtboard'; 
+import { PlayoutPage } from './pages/PlayoutPage'; 
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        {/* Debugging Artboard, Feel free to add your own components here for testing */}
-        <TestArtboard />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          {/* Default to Editor*/}
+          <Route path="/" element={<Navigate to="/editor" replace />} />
+          
+          {/* The Editor Zone */}
+          <Route path="/editor" element={<TestArtboard />} />
+          
+          {/*The Playout Engine*/}
+          <Route path="/playout" element={<PlayoutPage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
