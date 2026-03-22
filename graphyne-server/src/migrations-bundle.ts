ef459effb6292f0f81ce4ba1a526957d23ed0a52
Dilagante <dinethdds@gmail.com>
2026-03-22 12:43:19 +0530

    feat: Update dev script to include migration bundling and add Asset table migration

--- graphyne-server/src/migrations-bundle.ts
+++ graphyne-server/src/migrations-bundle.ts
@@ -54,4 +54,21 @@ CREATE TABLE "DataSource" (
 );
 `,
   },
+  {
+    name: "20260321195009_add_asset",
+    sql: `-- CreateTable
+CREATE TABLE "Asset" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "filePath" TEXT NOT NULL,
+    "fileSize" INTEGER NOT NULL,
+    "altText" TEXT,
+    "tags" TEXT NOT NULL DEFAULT '',
+    "uploadedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+`,
+  },
 ];
