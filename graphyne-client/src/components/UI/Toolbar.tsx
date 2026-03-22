3fd241153ebe454562c320ea3d078650ceba9e3f
Dilagante <dinethdds@gmail.com>
2026-03-22 14:13:11 +0530

    refactor: Remove unused asset panel dragging logic from Toolbar component

--- graphyne-client/src/components/UI/Toolbar.tsx
+++ graphyne-client/src/components/UI/Toolbar.tsx
@@ -23,10 +23,6 @@ import {
   ChevronDown,
    ImageUp,
 } from "lucide-react";
-import { useNavigate } from "react-router-dom";
-/* import { useDispatch, useSelector } from "react-redux";
-import { fetchAssets, setFilter, setSort, setSearch, deleteAsset, setEditingId } from "../../store/assetSlice";
-import type { RootState, AppDispatch } from "../../store/store"; */
 import AssetLibrary from "./AssetLibrary";
 
 export const Toolbar = () => {
@@ -40,31 +36,6 @@ export const Toolbar = () => {
 
   // --- Assets Library ---
   const [showAssetPanel, setShowAssetPanel] = useState(false);
-  const [panelPos, setPanelPos] = useState({ x: 120, y: 80 });
-  const dragOffset = useRef({ x: 0, y: 0 });
-  const isDragging = useRef(false);
-
-  const handlePanelMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
-    isDragging.current = true;
-    dragOffset.current = {
-      x: e.clientX - panelPos.x,
-      y: e.clientY - panelPos.y,
-    };
-    e.preventDefault();
-  };
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!isDragging.current) return;
-    setPanelPos({
-      x: e.clientX - dragOffset.current.x,
-      y: e.clientY - dragOffset.current.y,
-    });
-  };
-
-  const handleMouseUp = () => {
-    isDragging.current = false;
-  };
-
   // Hardcode snap size to 20 for newly added elements
   const snap = (value: number) =>
     grid.snap ? Math.round(value / 20) * 20 : value;
